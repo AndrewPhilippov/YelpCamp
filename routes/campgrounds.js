3bade63e42f2a74854ed69ba43464cc818f50d17
Andrew <a@fandb.pro>
2019-01-16 09:11:04 +0300

    refactoring lvl 1

---
+++ routes/campgrounds.js
@@ -0,0 +1,67 @@
+var express = require('express');
+var router  = express.Router();
+var Campground = require('../models/campground');
+
+// ============================
+// INDEX - show all campgrounds
+// ============================
+
+router.get('/', function(req,res){
+  // Get all campgrounds from DB
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render('campgrounds/index', { campgrounds: allCampgrounds });
+    }
+  })
+});
+
+// =================================
+// CREATE - add new campground to DB
+// =================================
+
+router.post('/', function(req, res){
+  // Get data from form and add to campgrounds array
+  var name = req.body.name;
+  var image = req.body.image;
+  var description = req.body.description;
+  var newCampground = {name: name, image: image, description: description};
+  // Create a new campground and save to database
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else {
+      // Redirect to campgrounds page
+      res.redirect('/campgrounds');
+    }
+  });
+});
+
+// ========================================
+// NEW - show form to create new campgorund
+// ========================================
+
+router.get('/new', function(req,res){
+  res.render('campgrounds/new');
+});
+
+// ==========
+// SHOW ROUTE
+// ==========
+
+router.get('/:id', function(req,res){
+  // Find the campgound with provided ID
+  Campground.findById(req.params.id).populate('comments').exec(function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      // Render show template with that campgound
+      res.render('campgrounds/show', {campground: foundCampground});
+    }
+  });
+});
+
+
+
+module.exports = router;
