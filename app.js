983fbdf5246bb1cde2b178992fe1348f59c2c272
Andrew <a@fandb.pro>
2019-01-19 15:41:29 +0300

    make it better

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use('/campgrounds/:id/comments',commentRoutes);
 app.use('/', indexRoutes);
 
 
-app.listen('8080', '127.0.0.1', function(){
+app.listen(process.env.PORT || '8080', process.env.ID || '127.0.0.1', function(){
   var date = new Date();
     console.log('Server is listening on \"8080\" at '+ date);
 });
